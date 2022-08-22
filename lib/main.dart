b8f1a39a10ea8d8504e740a2f48bc30d42ecbe0a
jjh5833 <jjh5833@naver.com>
2022-08-22 17:20:15 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
+
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
